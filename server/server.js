97ef9a9b5436ff9b736d8594ebfc424c72e9d6e6
Rohansadafule <sadafulerohan45@gmail.com>
2022-12-24 20:13:43 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const PORT = 4000
 const app = express()
 
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json())
 
 await mongoose.connect("mongodb+srv://rohansadafule:Newmernproject@cluster0.jomtob9.mongodb.net/?retryWrites=true&w=majority")
     console.log('MongoDB connection is succcessful.')
@@ -13,6 +15,11 @@ await mongoose.connect("mongodb+srv://rohansadafule:Newmernproject@cluster0.jomt
 app.get('/', (req, res) => {
     res.send('Hello server')
 });
+
+app.post('/transaction',(req,res)=>{
+    res.json({message:'New post request'});
+    const {amount,description,date}=req.body;
+})
 
 app.listen(PORT, () => {
     console.log('Server is running at port 4000')
